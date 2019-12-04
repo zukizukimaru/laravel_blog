1753097f0d87af7938f725c52543e713e359d1e6
zukizukimaru <kazucchi.98323@gmail.com>
2019-12-04 17:59:30 +0900

    indexの内容をサーバ上に表示させた

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  \App\Post;
 
 class PostsController extends Controller
 {
     public function index() {
-        return "hello";
+       // $posts =\App\Post::all();
+       // $posts =Post::all();
+       //$posts = Post::orderBy('created_at','desc')->get();
+       $posts = Post::latest()->get();
+        return view('index');
     }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8"> 
+<title>Blog Posts</title>
+<body>
+  <div class="container">
+   <h1>Blog Posts</h1>
+    <ul>
+      <li><a href="">title</a></li>
+      <li><a href="">title</a></li>
+      <li><a href="">title</a></li>
+    </ul>
+  </div>
+</body>
+</head>
