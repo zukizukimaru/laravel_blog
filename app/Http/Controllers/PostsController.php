e289a25a7bcaab3c06efc5baf024b0f1e5deb48c
zukizukimaru <kazucchi.98323@gmail.com>
2019-12-10 19:26:36 +0900

    エラーメッセージの内容を変更した

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use  \App\Post;
+use  App\Post;
+use  App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
@@ -30,11 +31,7 @@ class PostsController extends Controller
         return view('posts.create');
     }
 
-    public function store(Request $request) {
-        $this->validate($request, [
-            'title' => 'required|min:3',
-            'body' => 'required'
-        ]);
+    public function store(PostRequest $request) {
         $post = new Post();
         $post->title = $request->title;
         $post->body = $request->body;
@@ -46,11 +43,7 @@ class PostsController extends Controller
         return view('posts.edit')->with('post', $post);
       }
 
-      public function update(Request $request, Post $post) {
-        $this->validate($request, [
-            'title' => 'required|min:3',
-            'body' => 'required'
-        ]);
+      public function update(PostRequest $request, Post $post) {
         $post->title = $request->title;
         $post->body = $request->body;
         $post->save();
